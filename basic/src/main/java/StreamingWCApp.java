0c5cacfcbac1527f395104a4d0ec0632cf0c7b45
DebugWorld <23011873+DebugWorld-1024@users.noreply.github.com>
2023-01-15 15:18:50 +0800

    add word count

---
+++ basic/src/main/java/StreamingWCApp.java
@@ -0,0 +1,44 @@
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+
+public class StreamingWCApp {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<String> stringDataStreamSource = env.socketTextStream("127.0.0.1", 9527);
+        stringDataStreamSource.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String s, Collector<String> collector) throws Exception {
+                String[] worlds = s.split(",");
+                for (String world : worlds) {
+                    collector.collect(world.toLowerCase().trim());
+                }
+            }
+        }).filter(new FilterFunction<String>() {
+            @Override
+            public boolean filter(String s) throws Exception {
+                return StringUtils.isNotEmpty(s);
+            }
+        }).map(new MapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String s) throws Exception {
+                return new Tuple2<>(s, 1);
+            }
+        }).keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> stringIntegerTuple2) throws Exception {
+                return stringIntegerTuple2.f0;
+            }
+        }).sum(1).print();
+
+        env.execute("StreamingWCApp");
+    }
+}
